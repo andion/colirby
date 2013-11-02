bb5a59ab3bbd424c9eb2d9ecac22f4250d0f4749
Lucas Andión <andion@gmail.com>
2013-11-02 17:18:55 +0100

    Added webmock & vcr to testing environment

--- test/lib/colirby/version_test.rb
+++ test/lib/colirby/version_test.rb
@@ -2,7 +2,7 @@ require_relative '../../test_helper'
  
 describe Colirby do
  
-  it "VERSION must be defined" do
+  it "must have a VERSION defined" do
     Colirby::VERSION.wont_be_nil
   end
  
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,11 @@
+require File.expand_path('../../lib/colirby.rb', __FILE__)
+
 require 'minitest/autorun'
 require 'minitest/pride'
-require File.expand_path('../../lib/colirby.rb', __FILE__)
+require 'webmock/minitest'
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'test/fixtures/colirby_cassettes'
+  c.hook_into :webmock
+end
